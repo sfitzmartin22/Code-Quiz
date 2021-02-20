8f3488656ea4812ef8dce79a5078e46b51ec81a8
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-20 12:56:54 -0500

    local storage setup and all the functions to flor through the various visible sections of the page.  Timer is a little messed up when I miss questions as it will go negative but everything else works on it. negative timer, populating the high scores on the webpage and css sytling remain.

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,46 @@
 var secondsLeft;
 var timerEl = document.querySelector(".timer");
-var ViewHighScores = document.querySelector("#highscorelink");
-var highScores = document.querySelector("#highscore");
-var questionSet = document.querySelector(".questions");
+var highScoreList = document.querySelector("#highscore");
 var answersEl = document.querySelector("#answers");
 var startQ = document.querySelector("#question-container")
 var startB = document.querySelector("#start-btn")
 var currentQuestionArray = 0
-
+var submitR = document.querySelector("#results");
+var finalS = document.querySelector("#final-score")
+var submitRButton = document.querySelector("#submit");
+var userIDEl = document.querySelector("#userid")
+var HSL = document.querySelector("#highscorelink")
+var startOver = document.querySelector("#startover")
 
 startB.addEventListener("click", startGame)
+submitRButton.addEventListener("click", saveR)
+HSL.addEventListener("click", ViewHighScores)
+startOver.addEventListener("click", reload)
+
+
+function saveR() {
+    userID = userIDEl.value 
+    var highScores = JSON.parse(localStorage.getItem("highscores"))  || [];
+     var scoreObject = {
+         score: secondsLeft,
+         userID: userID
+     };
+
+     highScores.push(scoreObject);
+     localStorage.setItem("highscores", JSON.stringify(highScores));
+     submitR.setAttribute("class", "hide")
+     HSL.removeAttribute("class", "hide")
+    highScoreList.removeAttribute("class", "hide")
+    startOver.removeAttribute("class", "hide")
+
+ } 
+
+ function ViewHighScores(){
+
+    HSL.removeAttribute("class", "hide")
+    highScoreList.removeAttribute("class", "hide")
+    startOver.removeAttribute("class", "hide")
+ }
 
 function startGame () {
 startB.setAttribute("class", "hide")
@@ -18,30 +49,39 @@ timer();
 populateQuestion();
 }
 
+function reload() {
+    location.reload();
+}
 
 function questionClick() {
     if (this.value !== questionBucket[currentQuestionArray].answer && currentQuestionArray < 6) {
 
-    secondsLeft -= 10;
-
-    if (secondsLeft <0) {
-        secondsLeft === 0;
+    if (secondsLeft < 10) {
+        clearInterval(secondsLeft);
+        secondsLeft = 0;
+        timerEl.textContent = secondsLeft + " seconds remaining";
+        endQuiz;
+        return
+    } else {
+    secondsLeft-= 30;
+    }
     }
     timerEl.textContent = secondsLeft + " seconds remaining"; 
-    }
+ 
 
     currentQuestionArray++;
 
     if (currentQuestionArray === questionBucket.length) {  
+        clearInterval(timerStart);
         endQuiz();
     } else {
         populateQuestion();
-    }        
     }
+}        
 
 function endQuiz() {
-    clearInterval(secondsLeft);
     startQ.setAttribute("class", "hide");
+    submitR.removeAttribute("class", "hide");
 }
 
 
@@ -67,32 +107,32 @@ function populateQuestion(){
 var questionBucket = [
     {
         question: "What are variables used for in JavaScript",
-        choices: ["a. Storing numbers, dates, or other values", "b. Varying randomly", "c. All of the above", "d. None of the above"],
-        answer: "a. Storing numbers, dates, or other values"
+        choices: ["Storing numbers, dates, or other values", "Varying randomly", "All of the above", "None of the above"],
+        answer: "Storing numbers, dates, or other values"
     },
 
     {
         question: "What should appear at the very end of your JavaScript?",
-        choices: ["a. The <script>", "b. The END statement", "c. The </script>", "d. None of the above"],
-        answer: "c. The </script>"
+        choices: ["The <script>", "The END statement", "The </script>", "None of the above"],
+        answer: "The </script>"
     },
 
     {
         question: "Using _______ statement is how you test for a specific condition.",
-        choices: ["a. Select", "b. If", "c. Switch", "d. For"],
-        answer: "b. If"
+        choices: ["Select", "If", "Switch", "For"],
+        answer: "If"
     },
 
     {
         question: "The _______ method of an Array object adds and/or removes elements from an array.",
-        choices: ["a. Reverse", "b. Shift", "c. Splice", "d. Slice"],
-        answer: "c. Splice"
+        choices: ["Reverse", "Shift", "Splice", "Slice"],
+        answer: "Splice"
     },
 
     {
         question: "In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.",
-        choices: ["a. a wrapper", "b. a link", "c. a cursor", "d. a form"],
-        answer: "d. a form"
+        choices: ["a wrapper", "a link", "a cursor", "a form"],
+        answer: "a form"
     },
 
 ]
@@ -102,10 +142,9 @@ function timer() {
         timerStart = setInterval(function() {
             secondsLeft--;
             timerEl.textContent = secondsLeft + " seconds remaining";
-            if (secondsLeft === 0) {
+            if (secondsLeft === 0 || secondsLeft < 0 ) {
             clearInterval(timerStart);
-            timerEl.textContent = "Your time is up!"
-
+            endQuiz();
         }
     }, 1000);
 }
