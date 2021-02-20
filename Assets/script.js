41e8acae614e73b08bbba1046eb4649ed0cc8967
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-20 00:19:31 -0500

    question setup to roll through the question array and 10 second penalty. updated html to remove 4 buttons so that i could just append them in JS

--- Assets/script.js
+++ Assets/script.js
@@ -3,9 +3,11 @@ var timerEl = document.querySelector(".timer");
 var ViewHighScores = document.querySelector("#highscorelink");
 var highScores = document.querySelector("#highscore");
 var questionSet = document.querySelector(".questions");
-var answerSet = document.querySelector("#answer-buttons");
-var startQ = document.getElementById("question-container")
-var startB = document.getElementById("start-btn")
+var answersEl = document.querySelector("#answers");
+var startQ = document.querySelector("#question-container")
+var startB = document.querySelector("#start-btn")
+var currentQuestionArray = 0
+
 
 startB.addEventListener("click", startGame)
 
@@ -13,32 +15,84 @@ function startGame () {
 startB.setAttribute("class", "hide")
 startQ.removeAttribute("class", "hide")
 timer();
+populateQuestion();
+}
+
+
+function questionClick() {
+    if (this.value !== questionBucket[currentQuestionArray].answer && currentQuestionArray < 6) {
+
+    secondsLeft -= 10;
+
+    if (secondsLeft <0) {
+        secondsLeft === 0;
+    }
+    timerEl.textContent = secondsLeft + " seconds remaining"; 
+    }
+
+    currentQuestionArray++;
+
+    if (currentQuestionArray === questionBucket.length) {  
+        endQuiz();
+    } else {
+        populateQuestion();
+    }        
+    }
+
+function endQuiz() {
+    clearInterval(secondsLeft);
+    startQ.setAttribute("class", "hide");
+}
+
+
+
+function populateQuestion(){
+    var questionScreen = questionBucket[currentQuestionArray];
+    var titleEl = document.querySelector("#questions");
+    titleEl.textContent = questionScreen.question
+    answersEl.innerHTML = "";
+
+    questionScreen.choices.forEach(function (choice, i) {
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+        choiceButton.textContent = i + 1 + ". " + choice;
+        choiceButton.addEventListener("click", questionClick);
+
+        answersEl.appendChild(choiceButton);
+    });
+
 }
 
 var questionBucket = [
     {
         question: "What are variables used for in JavaScript",
-        choices: [{text: "a. Storing numbers, dates, or other values", correct: true}, {text: "b. Varying randomly", correct: false}, {text: "c. All of the above", correct: false}, {text: "d. None of the above", correct: false}],
+        choices: ["a. Storing numbers, dates, or other values", "b. Varying randomly", "c. All of the above", "d. None of the above"],
+        answer: "a. Storing numbers, dates, or other values"
     },
 
     {
         question: "What should appear at the very end of your JavaScript?",
-        choices: [{text: "a. The <script>", correct: false}, {text: "b. The END statement", correct: false}, {text: "c. The </script>", correct: true}, {text: "d. None of the above", correct: false}],
+        choices: ["a. The <script>", "b. The END statement", "c. The </script>", "d. None of the above"],
+        answer: "c. The </script>"
     },
 
     {
         question: "Using _______ statement is how you test for a specific condition.",
-        choices: [{text: "a. Select", correct: false}, {text: "b. If", correct: true}, {text: "c. Switch", correct: false}, {text: "d. For", correct: false}],
+        choices: ["a. Select", "b. If", "c. Switch", "d. For"],
+        answer: "b. If"
     },
 
     {
         question: "The _______ method of an Array object adds and/or removes elements from an array.",
-        choices: [{text: "a. Reverse", correct: false}, {text: "b. Shift", correct: false}, {text: "c. Splice", correct: true}, {text: "d. Slice", correct: false}],
+        choices: ["a. Reverse", "b. Shift", "c. Splice", "d. Slice"],
+        answer: "c. Splice"
     },
 
     {
         question: "In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.",
-        choices: [{text: "a. a wrapper", correct: true}, {text: "b. a link", correct: false}, {text: "c. a cursor", correct: false}, {text: "d. a form", correct: true}]
+        choices: ["a. a wrapper", "b. a link", "c. a cursor", "d. a form"],
+        answer: "d. a form"
     },
 
 ]
